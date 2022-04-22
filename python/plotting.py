246fd84f95db6e8716eb9ad9fd7461cef7293a10
Jannis Schönleber <joennlae@gmail.com>
2022-04-22 13:55:56 +0200

    data(conv1x1): all layers + C=128 and C=96 for some layers

--- python/plotting.py
+++ python/plotting.py
@@ -15,12 +15,20 @@ def json_to_dataframe(path: str, layer_name: str) -> pd.DataFrame:
     dfs = []  # an empty list to store the data frames
     for file in files_res:
         data = pd.read_json(file)  # read data frame from json file
-        if layer_name + ".learned_a_shape" in data.columns:
-            data["learned_n"] = data.iloc[0][layer_name + ".learned_a_shape"]
-            data["learned_d"] = data.iloc[1][layer_name + ".learned_a_shape"]
+        if layer_name + ".learned_n" not in data.columns:
+            data[layer_name + ".learned_n"] = data.iloc[0][
+                layer_name + ".learned_a_shape"
+            ]
+            data[layer_name + ".learned_d"] = data.iloc[1][
+                layer_name + ".learned_a_shape"
+            ]
             K = data.iloc[0][layer_name + ".K"]
             C = data.iloc[0][layer_name + ".C"]
-            data["learned_m"] = int(data.iloc[0][layer_name + ".L_size"] / (4 * K * C))
+            data[layer_name + ".learned_m"] = int(
+                data.iloc[0][layer_name + ".L_size"] / (4 * K * C)
+            )
+
+        if layer_name + ".learned_a_shape" in data.columns:
             data = data.drop([1])
             data = data.drop(
                 columns=[
@@ -38,24 +46,28 @@ def json_to_dataframe(path: str, layer_name: str) -> pd.DataFrame:
     df = pd.concat(
         dfs, ignore_index=True
     )  # concatenate all the data frames in the list.
+
     df = df.drop(columns="halut_layers")
+    df["top_1_accuracy_100"] = df["top_1_accuracy"] * 100
     df.columns = df.columns.str.replace(layer_name + ".", "")
     df.sort_values(
         by=["rows", "test_name"], inplace=True, ignore_index=True, ascending=False
     )
-    df["top_1_accuracy_100"] = df["top_1_accuracy"] * 100
+    # df = df.reindex(sorted(df.columns), axis=1)
     return df
 
 
 layers = [
     "layer1.0.conv1",
     "layer1.0.conv3",
+    "layer1.0.downsample.0",
     "layer1.1.conv1",
     "layer1.1.conv3",
     "layer1.2.conv1",
     "layer1.2.conv3",
     "layer2.0.conv1",
     "layer2.0.conv3",
+    "layer2.0.downsample.0",
     "layer2.1.conv1",
     "layer2.1.conv3",
     "layer2.2.conv1",
@@ -64,6 +76,7 @@ layers = [
     "layer2.3.conv3",
     "layer3.0.conv1",
     "layer3.0.conv3",
+    "layer3.0.downsample.0",
     "layer3.1.conv1",
     "layer3.1.conv3",
     "layer3.2.conv1",
@@ -76,10 +89,18 @@ layers = [
     "layer3.5.conv3",
     "layer4.0.conv1",
     "layer4.0.conv3",
+    "layer4.0.downsample.0",
     "layer4.1.conv1",
     "layer4.1.conv3",
     "layer4.2.conv1",
     "layer4.2.conv3",
+]
+
+downsampled = [
+    "layer1.0.downsample.0",
+    "layer2.0.downsample.0",
+    "layer3.0.downsample.0",
+    "layer4.0.downsample.0",
 ]
 
 if __name__ == "__main__":
@@ -115,6 +136,9 @@ if __name__ == "__main__":
         # {"font.family": "serif", "font.serif": ["Times", "Palatino", "serif"]},
     )
     colors = sns.color_palette("Greys", n_colors=5).as_hex()[-3:]
+    colors2 = sns.color_palette("Blues", n_colors=5).as_hex()[2:4]
+    colors = colors + colors2
+    print(colors, colors2)
     # colors.reverse()
     customPalette = sns.color_palette(colors)
     grid = sns.FacetGrid(
@@ -131,7 +155,7 @@ if __name__ == "__main__":
     grid.refline(y=RESNET_ACC, linestyle=":", color="red")
     # grid.refline(y=80.0, linestyle=":", color="grey")
     # grid.refline(y=79.0, linestyle=":", color="grey")
-    MIN = 77.95
+    MIN = 63.95
     # Draw a line plot to show the trajectory of each random walk
     grid.map(plt.plot, "rows", "top_1_accuracy_100", marker="o")
     for (row_i, col_j, hue_k), data_ijk in grid.facet_data():
@@ -144,7 +168,7 @@ if __name__ == "__main__":
             xmax=((40 * 256) + 1),
             linestyle="--",
         )
-        if data_ijk.iloc[0]["C"] == 64:
+        if data_ijk.shape[0] and data_ijk.iloc[0]["C"] == 64:
             error = np.min(data_ijk["scaled_error"])
             C = data_ijk.iloc[0]["C"]
             K = data_ijk.iloc[0]["K"]
@@ -152,14 +176,29 @@ if __name__ == "__main__":
             M = data_ijk.iloc[0]["learned_m"]
             representation_factor = C * np.log2(K) / D
             uncertainty_factor = int(np.log2(M / representation_factor))
+            lut_size = data_ijk.iloc[0]["L_size"] / 1024 / 1024
             ax.text(
                 y=MIN + 0.1,
                 x=1,
                 s=(
                     f"{max_acc:0.2f}% (-{RESNET_ACC - max_acc:0.2f}%) |"
-                    rf" {error:.2e} | {uncertainty_factor}"
+                    rf" {error:.2e} | {lut_size:0.1f} MB | {uncertainty_factor}"
                 ),
-                fontsize=8.4,
+                fontsize=7,
+            )
+
+        if data_ijk.shape[0] and data_ijk.iloc[0]["C"] == 128:
+            error = np.min(data_ijk["scaled_error"])
+            lut_size = data_ijk.iloc[0]["L_size"] / 1024 / 1024
+            ax.text(
+                y=MIN + (RESNET_ACC - MIN) / 10,
+                x=1,
+                s=(
+                    f"{max_acc:0.2f}% (-{RESNET_ACC - max_acc:0.2f}%) |"
+                    rf" {error:.2e} | {lut_size:0.1f} MB"
+                ),
+                color=grid._facet_color(hue_k, None),
+                fontsize=7,
             )
 
     grid.set_axis_labels("#Images for Halut", "Top1 Accuracy (%)")
